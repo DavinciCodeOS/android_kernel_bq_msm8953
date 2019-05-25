e9ddb2882a5c8a40551cc89d74010f9e07e8f2ff
Wei Wang <wvw@google.com>
2019-05-24 19:18:36 -0700

    Revert "sched: Improve the scheduler"
    This reverts commit a3dd94a1bb80ec98924070f28ba80d93a4d559a6.
    
    Bug:133481659
    Test: build
    Change-Id: Ib23609315f3446223521612621fe54469537c172
    Signed-off-by: Wei Wang <wvw@google.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -949,8 +949,6 @@ unsigned int cpufreq_generic_get(unsigned int cpu);
 int cpufreq_generic_init(struct cpufreq_policy *policy,
 		struct cpufreq_frequency_table *table,
 		unsigned int transition_latency);
-
-extern unsigned int cpuinfo_max_freq_cached;
 struct sched_domain;
 unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 unsigned long cpufreq_scale_max_freq_capacity(struct sched_domain *sd, int cpu);
